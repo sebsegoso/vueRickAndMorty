216bfee64f9e0419f9275b729ee1c63c5d86f07b
Sebastián Segura Osorio <sseguraosorio@gmail.com>
2020-09-05 18:59:13 -0400

    primer commit , componentes y lógica lista, props

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Componente padre</h1>
+    <Personaje v-for="(personaje, i) in personajes" :key="i" :fotoPersonaje="personaje.img" :nombrePersonaje="personaje.nombre" />
+    <!-- Mensaje error -->
+    <p
+      v-if="error"
+    >Lo sentimos, no hemos podido acceder a los datos solicitados en este momento, intente nuevamente.</p>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Personaje from "./components/Personaje";
+import axios from "axios";
 export default {
-  name: 'App',
+  name: "RickAndMorty",
   components: {
-    HelloWorld
-  }
-}
+    Personaje,
+  },
+  data() {
+    return {
+      personajes: [],
+      error: false
+    };
+  },
+  mounted() {
+    axios
+      .get(`https://rickandmortyapi.com/api/character/?page=1`)
+      .then((res) => {
+        let data = res.data.results;
+        console.log(data);
+        data.forEach((personaje) => {
+          let nombre = personaje.name;
+          let img = personaje.image;
+
+          this.personajes.push({ nombre, img });
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        this.error = true;
+      });
+  },
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  box-sizing: content-box;
+}
+
+body {
+  min-height: 100vh;
+  background-color: #000;
+  background-image: url(https://rickandmortypod.com/wp-content/uploads/2018/11/cropped-RM_page-header_background1-3.png);
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  color: #97ce4c;
 }
 </style>
---
+++ src/components/Personaje.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="card">
+      <img :src="fotoPersonaje" alt="foto personaje" />
+      <h3>{{nombrePersonaje}}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Personaje",
+  props: {
+    fotoPersonaje: String,
+    nombrePersonaje: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+body {
+  .card {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    color: #000;
+    background-color: #97ce4c;
+
+    width: 35%;
+    padding: 20px 0;
+    border-radius: 15px;
+    img {
+      border-radius: 50%;
+      max-width: 40%;
+    }
+  }
+}
+</style>
